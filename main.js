4e4703cf44cfed061891f11d6a7d12a636f12f2c
Thomas McPhee <mcp.thom@gmail.com>
2023-10-29 15:30:32 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+// let spans = document.querySelectorAll('span');
+console.log(document)
+
+let index = 0
+for (each of spans) {
+    each.style.color = colors[index];
+    index += 1
+};
